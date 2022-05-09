7f4702ea02ffadc8b174b757cb16c33b807a2864
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-09 09:05:49 +0530

    Added For loop code in course 1

---
+++ src/course_1_Intro_Java/loops/ForLoop.java
@@ -0,0 +1,15 @@
+package course_1_Intro_Java.loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1,2,3,4,5};
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println("arr element at index "+i+" is "+arr[i]);
+		}
+	}
+
+}
