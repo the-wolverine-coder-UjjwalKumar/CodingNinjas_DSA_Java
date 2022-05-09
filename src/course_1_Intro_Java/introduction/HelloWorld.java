dc57f8ba32fcf70e43cadc6fa038c0447ac94888
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-09 09:05:27 +0530

    Added Hello world code

---
+++ src/course_1_Intro_Java/introduction/HelloWorld.java
@@ -0,0 +1,10 @@
+package course_1_Intro_Java.introduction;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Ujjwal In the world of Java !!");
+	}
+
+}
