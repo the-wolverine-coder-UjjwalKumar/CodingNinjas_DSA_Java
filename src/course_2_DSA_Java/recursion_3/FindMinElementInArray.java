16bdb803799c690e681ff254f0fa406e09fda284
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-10 09:52:39 +0530

    Recursion-3 updated find min element in arr code

--- src/course_2_DSA_Java/recursion_3/FindMinElementInArray.java
+++ src/course_2_DSA_Java/recursion_3/FindMinElementInArray.java
@@ -2,9 +2,19 @@ package course_2_DSA_Java.recursion_3;
 
 public class FindMinElementInArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int findMin(int arr[], int n) {
+		
+		if (n == 1)
+			return arr[0];
 
+		return Math.min(arr[n - 1], findMin(arr, n - 1));
+	}
+
+	public static void main(String args[]) {
+		int A[] = { 1, 4, 45, 6, -50, 10, 2 };
+		int n = A.length;
+
+		System.out.println(findMin(A, n));
 	}
 
 }
