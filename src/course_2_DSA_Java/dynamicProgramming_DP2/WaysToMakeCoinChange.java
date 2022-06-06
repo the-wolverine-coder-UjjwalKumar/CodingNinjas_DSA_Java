7d2551cbc5ac6f5f1249c397df8ef79b31b17962
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-06 22:10:44 +0530

    Added Solution Zero One Knapsack

---
+++ src/course_2_DSA_Java/dynamicProgramming_DP2/WaysToMakeCoinChange.java
@@ -0,0 +1,33 @@
+package course_2_DSA_Java.dynamicProgramming_DP2;
+
+public class WaysToMakeCoinChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int countWaysToMakeChange(int denominations[], int value) {
+		// Write your code here
+		/*
+		 * if (value==0) return 1;
+		 * 
+		 * if (value<0) return 0;
+		 * 
+		 * int finalAns=0; for (int i=0;i<denominations.length;i++) {
+		 * finalAns=finalAns+countWaysToMakeChange(denominations,value-denominations[i])
+		 * ; } return finalAns;
+		 */
+		int[] dp = new int[value + 1];
+		dp[0] = 1;
+		for (int i = 0; i < denominations.length; i++) {
+			for (int j = 0; j <= value; j++) {
+				if (j >= denominations[i]) {
+					dp[j] = dp[j] + dp[j - denominations[i]];
+				}
+			}
+		}
+		return dp[value];
+	}
+
+}
